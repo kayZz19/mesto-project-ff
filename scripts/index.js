26327e01c409a10f33b91d921018f49689243cd4
Roman Kirillov <roman.kirillov.06@mail.ru>
2025-03-18 12:32:02 +0300

    firsttry

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+// @todo: Темплейт карточки
+
+// @todo: DOM узлы
+
+// @todo: Функция создания карточки
+
+// @todo: Функция удаления карточки
+
+// @todo: Вывести карточки на страницу
+
+function createCard(cardData, deleteCallback) {
+  const cardTemplate = document.getElementById("card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+const placesList = document.querySelector(".places__list");
+
+initialCards.forEach((card) => {
+  const newCard = createCard(card, deleteCard);
+  placesList.appendChild(newCard);
+});
